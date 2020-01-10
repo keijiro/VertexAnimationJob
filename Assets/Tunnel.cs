23f56b78d6519ba6f96b0b8d29a4053c5847bdd1
Keijiro Takahashi <keijiro@gmail.com>
2020-01-10 16:54:24 +0900

    Added old API implementation

--- Assets/Tunnel.cs
+++ Assets/Tunnel.cs
@@ -3,12 +3,15 @@
 // https://github.com/keijiro/VertexAnimationJob
 //
 
+//#define USE_NEW_API
+
 using UnityEngine;
 using UnityEngine.Rendering;
 using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Random = Unity.Mathematics.Random;
+using System.Collections.Generic;
 
 [ExecuteInEditMode, RequireComponent(typeof(MeshRenderer))]
 public sealed class Tunnel : MonoBehaviour
@@ -35,8 +38,15 @@ public sealed class Tunnel : MonoBehaviour
     #region Internal objects
 
     Mesh _mesh;
+
+#if USE_NEW_API
     NativeArray<int> _indexBuffer;
     NativeArray<float3> _vertexBuffer;
+#else
+    int[] _indexBuffer = new int [0];
+    Vector3[] _vertexBuffer;
+    Vector3[] _normalBuffer;
+#endif
 
     #endregion
 
@@ -130,23 +140,32 @@ public sealed class Tunnel : MonoBehaviour
 
     void AllocateBuffers()
     {
+#if USE_NEW_API
         _indexBuffer = new NativeArray<int>(
             IndexCount, Allocator.Persistent,
             NativeArrayOptions.UninitializedMemory
         );
-
         _vertexBuffer = new NativeArray<float3>(
             VertexCount * 2, Allocator.Persistent,
             NativeArrayOptions.UninitializedMemory
         );
+#else
+        _indexBuffer = new int [IndexCount];
+        _vertexBuffer = new Vector3 [VertexCount];
+        _normalBuffer = new Vector3 [VertexCount];
+#endif
 
         InitializeIndexArray();
     }
 
     void DisposeBuffers()
     {
+#if USE_NEW_API
         if (_indexBuffer.IsCreated) _indexBuffer.Dispose();
         if (_vertexBuffer.IsCreated) _vertexBuffer.Dispose();
+#else
+        _indexBuffer = new int [0];
+#endif
     }
 
     void InitializeIndexArray()
@@ -187,6 +206,7 @@ public sealed class Tunnel : MonoBehaviour
 
     void InitializeMesh()
     {
+#if USE_NEW_API
         _mesh.SetVertexBufferParams(
             VertexCount,
             new VertexAttributeDescriptor
@@ -200,16 +220,27 @@ public sealed class Tunnel : MonoBehaviour
         _mesh.SetIndexBufferData(_indexBuffer, 0, 0, IndexCount);
 
         _mesh.SetSubMesh(0, new SubMeshDescriptor(0, IndexCount));
+#else
+        _mesh.SetVertices(_vertexBuffer);
+        _mesh.SetNormals(_normalBuffer);
+        _mesh.SetIndices(_indexBuffer, MeshTopology.Triangles, 0);
+#endif
     }
 
     void UpdateVerticesOnMesh()
     {
+#if USE_NEW_API
         _mesh.SetVertexBufferData(_vertexBuffer, 0, 0, VertexCount * 2);
+#else
+        _mesh.SetVertices(_vertexBuffer);
+#endif
     }
 
     #endregion
 
     #region Jobified vertex animation
+
+#if USE_NEW_API
 
     void UpdateVertexBuffer()
     {
@@ -291,6 +322,63 @@ public sealed class Tunnel : MonoBehaviour
             buffer[i * 2 + 1] = -N;
         }
     }
+
+#else
+
+    // Calculate a vertex position from polar coordinates and a
+    // displacement value.
+    float3 CalculateVertex(float2 polar, float disp)
+    {
+        var theta = math.PI * 2 * polar.x;
+        var r = math.float2(math.cos(theta), math.sin(theta));
+        var z = (polar.y - 0.5f) * _depth;
+        return math.float3(r * (_radius + disp), z);
+    }
+
+    void UpdateVertexBuffer()
+    {
+        var noiseRot = Time.time * _noiseAnimation;
+
+        for (var i = 0; i < VertexCount; i++)
+        {
+            // Index -> Polar coodinates
+            var polar = math.float2(
+                math.frac((float)i / _resolution.x),
+                (float)(i / _resolution.x) / _resolution.y
+            );
+
+            // Polar coodinates -> Noise position
+            // The offset (1, 0) is needed for correct tiling.
+            var npos = (polar + math.float2(1, 0)) *
+                math.float2(1, _depth / (math.PI * 2 * _radius)) * _noiseRepeat;
+
+            // Noise field repeating period
+            var nrep = math.float2(_noiseRepeat, 1000);
+
+            // Noise sample with analytical gradients
+            var psrdn = noise.psrdnoise(npos, nrep, noiseRot) * _noiseAmplitude;
+            var disp = psrdn.x;
+            var grad = psrdn.yz;
+
+            // Vertex position
+            var V = CalculateVertex(polar, disp);
+
+            // Normal calculation using the gradients
+            var D = 0.001f;
+            var Dp = D / _noiseRepeat;
+            var Vdx0 = CalculateVertex(polar - math.float2(Dp, 0), disp - grad.x * D);
+            var Vdx1 = CalculateVertex(polar + math.float2(Dp, 0), disp + grad.x * D);
+            var Vdy0 = CalculateVertex(polar - math.float2(0, Dp), disp - grad.y * D);
+            var Vdy1 = CalculateVertex(polar + math.float2(0, Dp), disp + grad.y * D);
+            var N = math.normalize(math.cross(Vdx1 - Vdx0, Vdy1 - Vdy0));
+
+            // Output
+            _vertexBuffer[i] = V;
+            _normalBuffer[i] = -N;
+        }
+    }
+
+#endif
 
     #endregion
 }
